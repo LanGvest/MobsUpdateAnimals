243ea13e703f6a936323c5769bb074b1acacf516
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2022-05-09 16:04:03 +0300

    Add proxies

--- src/main/java/langvest/mobsupdateanimals/MobsUpdateAnimals.java
+++ src/main/java/langvest/mobsupdateanimals/MobsUpdateAnimals.java
@@ -1,19 +1,30 @@
 package langvest.mobsupdateanimals;
 
 import langvest.mobsupdateanimals.core.Config;
+import langvest.mobsupdateanimals.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Config.MOD_ID, name = Config.MOD_NAME, version = Config.MOD_VERSION, acceptedMinecraftVersions = Config.MC_VERSION)
 public class MobsUpdateAnimals {
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {}
+    @SidedProxy(clientSide = Config.MOD_PROXY_CLIENT, serverSide = Config.MOD_PROXY_COMMON)
+    public static CommonProxy proxy;
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {}
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event) {}
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
 }
--- src/main/java/langvest/mobsupdateanimals/core/Config.java
+++ src/main/java/langvest/mobsupdateanimals/core/Config.java
@@ -6,4 +6,7 @@ public class Config {
     public static final String MOD_GROUP = "langvest.mobsupdateanimals";
     public static final String MOD_VERSION = "1.0.0";
     public static final String MC_VERSION = "1.12.2";
+
+    public static final String MOD_PROXY_COMMON = Config.MOD_GROUP + ".proxy.CommonProxy";
+    public static final String MOD_PROXY_CLIENT = Config.MOD_GROUP + ".proxy.ClientProxy";
 }
