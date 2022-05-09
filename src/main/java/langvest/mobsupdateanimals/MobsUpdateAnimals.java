fd1611054b07f1fabbe0fb236bcb12431f2944a9
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2022-05-09 15:55:14 +0300

    Add config

--- src/main/java/langvest/mobsupdateanimals/MobsUpdateAnimals.java
+++ src/main/java/langvest/mobsupdateanimals/MobsUpdateAnimals.java
@@ -1,11 +1,12 @@
 package langvest.mobsupdateanimals;
 
+import langvest.mobsupdateanimals.core.Config;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "mobsupdateanimals")
+@Mod(modid = Config.MOD_ID, name = Config.MOD_NAME, version = Config.MOD_VERSION, acceptedMinecraftVersions = Config.MC_VERSION)
 public class MobsUpdateAnimals {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {}
---
+++ src/main/java/langvest/mobsupdateanimals/core/Config.java
@@ -0,0 +1,9 @@
+package langvest.mobsupdateanimals.core;
+
+public class Config {
+    public static final String MOD_ID = "mobsupdateanimals";
+    public static final String MOD_NAME = "Mobs Update: Animals";
+    public static final String MOD_GROUP = "langvest.mobsupdateanimals";
+    public static final String MOD_VERSION = "1.0.0";
+    public static final String MC_VERSION = "1.12.2";
+}
