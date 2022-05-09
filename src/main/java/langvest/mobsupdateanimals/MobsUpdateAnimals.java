2e8e360307d20ea4cda5426f1c9deed2bbf9c419
Viacheslav Logvinets <7425816-LanGvest@users.noreply.gitlab.com>
2022-05-09 15:40:57 +0300

    Initial commit

---
+++ src/main/java/langvest/mobsupdateanimals/MobsUpdateAnimals.java
@@ -0,0 +1,18 @@
+package langvest.mobsupdateanimals;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "mobsupdateanimals")
+public class MobsUpdateAnimals {
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {}
+}
